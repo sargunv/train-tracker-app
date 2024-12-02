695c354ede0a831cdd238c5bda06a6e2ca6accca
Tobias Zwick <newton@westnordost.de>
2024-12-02 21:59:46 +0100

    Lint
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- lib/maplibre-compose/src/commonMain/kotlin/dev/sargunv/maplibrecompose/core/GestureSettings.kt
+++ lib/maplibre-compose/src/commonMain/kotlin/dev/sargunv/maplibrecompose/core/GestureSettings.kt
@@ -3,18 +3,18 @@ package dev.sargunv.maplibrecompose.core
 import androidx.compose.runtime.Immutable
 
 /**
- *  Defines which user map gestures are enabled
+ * Defines which user map gestures are enabled
  *
- *  @param isTiltGesturesEnabled whether user may tilt (pitch) the map by vertically dragging two
- *    fingers up or down
- *  @param isZoomGesturesEnabled whether user may zoom the map in and out by pinching two fingers or
- *    by double tapping, holding, and moving the finger up and down as well as simply double tapping
- *    to zoom in
- *  @param isRotateGesturesEnabled whether user may rotate the map by moving two fingers in a
- *    circular motion
- *  @param isScrollGesturesEnabled whether user may scroll the map by dragging or swiping with one
- *    finger
- *  */
+ * @param isTiltGesturesEnabled whether user may tilt (pitch) the map by vertically dragging two
+ *   fingers up or down
+ * @param isZoomGesturesEnabled whether user may zoom the map in and out by pinching two fingers or
+ *   by double tapping, holding, and moving the finger up and down as well as simply double tapping
+ *   to zoom in
+ * @param isRotateGesturesEnabled whether user may rotate the map by moving two fingers in a
+ *   circular motion
+ * @param isScrollGesturesEnabled whether user may scroll the map by dragging or swiping with one
+ *   finger
+ */
 @Immutable
 public data class GestureSettings(
   val isTiltGesturesEnabled: Boolean = true,
--- lib/maplibre-compose/src/commonMain/kotlin/dev/sargunv/maplibrecompose/core/OrnamentSettings.kt
+++ lib/maplibre-compose/src/commonMain/kotlin/dev/sargunv/maplibrecompose/core/OrnamentSettings.kt
@@ -6,20 +6,20 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.unit.dp
 
 /**
- *  Defines which additional UI elements are displayed on top of the map.
+ * Defines which additional UI elements are displayed on top of the map.
  *
- *  @param padding padding of the ornaments to the edge of the map
- *  @param isLogoEnabled whether to display the MapLibre logo
- *  @param logoAlignment where to place the MapLibre logo
- *  @param isAttributionEnabled whether to display copyright attribution information. If `true`,
- *    all attribution for all sources used is displayed. If there is not enough space to
- *    conveniently display those, a small button in the shape of an ⓘ will be displayed instead.
- *    Tapping on it opens the attribution information in a dialog.
- *  @param attributionAlignment where to place the attribution information
- *  @param isCompassEnabled whether to display a compass that shows which direction is north.
- *    Tapping on the compass will reset the camera rotations to zero.
- *  @param compassAlignment where to place the compass
- * */
+ * @param padding padding of the ornaments to the edge of the map
+ * @param isLogoEnabled whether to display the MapLibre logo
+ * @param logoAlignment where to place the MapLibre logo
+ * @param isAttributionEnabled whether to display copyright attribution information. If `true`, all
+ *   attribution for all sources used is displayed. If there is not enough space to conveniently
+ *   display those, a small button in the shape of an ⓘ will be displayed instead. Tapping on it
+ *   opens the attribution information in a dialog.
+ * @param attributionAlignment where to place the attribution information
+ * @param isCompassEnabled whether to display a compass that shows which direction is north. Tapping
+ *   on the compass will reset the camera rotations to zero.
+ * @param compassAlignment where to place the compass
+ */
 @Stable
 public data class OrnamentSettings(
   val padding: PaddingValues = PaddingValues(0.dp),
