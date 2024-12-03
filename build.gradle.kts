51e90b96802a6f130ed5ce0069dad938b016c519
Sargun Vohra <sargun.vohra@gmail.com>
2024-12-02 18:18:03 -0800

    install mkdocs

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,9 +9,15 @@ plugins {
   alias(libs.plugins.kotlin.cocoapods) apply false
   alias(libs.plugins.spotless)
   alias(libs.plugins.dokka)
+  alias(libs.plugins.jgitver)
+  alias(libs.plugins.mkdocs)
 }
 
-dokka { moduleName = "MapLibre Compose" }
+jgitver { nonQualifierBranches("main") }
+
+mkdocs { sourcesDir = "docs" }
+
+dokka { moduleName = "MapLibre Compose API Reference" }
 
 dependencies { dokka(project(":lib:maplibre-compose:")) }
 
