c44f76f1a12be65cda0109780f86ac1a6d17f764
Sargun Vohra <sargun.vohra@gmail.com>
2024-12-06 00:21:02 -0800

    refactor demo app

--- demo-app/src/commonMain/kotlin/dev/sargunv/maplibrecompose/demoapp/demos/AnimatedLayerDemo.kt
+++ demo-app/src/commonMain/kotlin/dev/sargunv/maplibrecompose/demoapp/demos/AnimatedLayerDemo.kt
@@ -4,10 +4,8 @@ import androidx.compose.animation.animateColor
 import androidx.compose.animation.core.infiniteRepeatable
 import androidx.compose.animation.core.keyframes
 import androidx.compose.animation.core.rememberInfiniteTransition
-import androidx.compose.foundation.layout.Column
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import dev.sargunv.maplibrecompose.compose.MaplibreMap
@@ -18,7 +16,7 @@ import dev.sargunv.maplibrecompose.compose.source.rememberGeoJsonSource
 import dev.sargunv.maplibrecompose.core.CameraPosition
 import dev.sargunv.maplibrecompose.demoapp.DEFAULT_STYLE
 import dev.sargunv.maplibrecompose.demoapp.Demo
-import dev.sargunv.maplibrecompose.demoapp.SimpleDemoScaffold
+import dev.sargunv.maplibrecompose.demoapp.DemoScaffold
 import dev.sargunv.maplibrecompose.demoapp.generated.Res
 import io.github.dellisd.spatialk.geojson.Position
 import org.jetbrains.compose.resources.ExperimentalResourceApi
@@ -34,39 +32,36 @@ object AnimatedLayerDemo : Demo {
   @Composable
   @OptIn(ExperimentalResourceApi::class)
   override fun Component(navigateUp: () -> Unit) {
-    SimpleDemoScaffold(this, navigateUp = { navigateUp() }) {
-      Column {
-        MaplibreMap(
-          modifier = Modifier.weight(1f),
-          styleUrl = DEFAULT_STYLE,
-          cameraState = rememberCameraState(firstPosition = CameraPosition(target = US, zoom = 2.0)),
-        ) {
-          val routeSource =
-            rememberGeoJsonSource(id = "amtrak-routes", dataUrl = Res.getUri(ROUTES_FILE))
+    DemoScaffold(this, navigateUp) {
+      MaplibreMap(
+        styleUrl = DEFAULT_STYLE,
+        cameraState = rememberCameraState(firstPosition = CameraPosition(target = US, zoom = 2.0)),
+      ) {
+        val routeSource =
+          rememberGeoJsonSource(id = "amtrak-routes", dataUrl = Res.getUri(ROUTES_FILE))
 
-          val infiniteTransition = rememberInfiniteTransition()
-          val animatedColor by
-            infiniteTransition.animateColor(
-              Color.hsl(0f, 1f, 0.5f),
-              Color.hsl(0f, 1f, 0.5f),
-              animationSpec =
-                infiniteRepeatable(
-                  animation =
-                    keyframes {
-                      durationMillis = 10000
-                      for (i in 1..9) Color.hsl(i * 36f, 1f, 0.5f) at (i * 1000)
-                    }
-                ),
-            )
+        val infiniteTransition = rememberInfiniteTransition()
+        val animatedColor by
+          infiniteTransition.animateColor(
+            Color.hsl(0f, 1f, 0.5f),
+            Color.hsl(0f, 1f, 0.5f),
+            animationSpec =
+              infiniteRepeatable(
+                animation =
+                  keyframes {
+                    durationMillis = 10000
+                    for (i in 1..9) Color.hsl(i * 36f, 1f, 0.5f) at (i * 1000)
+                  }
+              ),
+          )
 
-          Anchor.Below("waterway_line_label") {
-            LineLayer(
-              id = "amtrak-routes",
-              source = routeSource,
-              color = const(animatedColor),
-              width = const(4.dp),
-            )
-          }
+        Anchor.Below("waterway_line_label") {
+          LineLayer(
+            id = "amtrak-routes",
+            source = routeSource,
+            color = const(animatedColor),
+            width = const(4.dp),
+          )
         }
       }
     }
